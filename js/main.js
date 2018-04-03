9589d00ed2e84025d36f55bb0a72b465e3aeadc9
Vadim Korabelnik <vad@oblako9.ru>
2018-04-03 18:21:12 +0300

    Added modules

--- js/main.js
+++ js/main.js
@@ -1,39 +1,4 @@
-{
-  const KEY_CODE_RIGHT = 39;
-  const KEY_CODE_LEFT = 37;
-
-  const centralContainer = document.querySelector(`.central`);
-  const templates = Array.from(document.querySelectorAll(`template`));
-  let defaultIndex = 0;
-
-  const showSlide = (slide) => {
-    const content = slide.content;
-    const clone = content.cloneNode(true);
-
-    centralContainer.innerHTML = ``;
-    centralContainer.appendChild(clone);
-  };
-
-  const changeSlide = (ev) => {
-    if (ev.altKey && ev.keyCode === KEY_CODE_RIGHT) {
-      defaultIndex++;
-
-      if (defaultIndex % templates.length === 0) {
-        defaultIndex--;
-      }
-    }
-
-    if (ev.altKey && ev.keyCode === KEY_CODE_LEFT) {
-      defaultIndex--;
-
-      if (defaultIndex === -1) {
-        defaultIndex++;
-      }
-    }
-    showSlide(templates[defaultIndex]);
-  };
-
-  document.addEventListener(`keydown`, changeSlide);
-  showSlide(templates[defaultIndex]);
-}
-
+import {makeIntroTemplate} from './modules/templates/intro.js';
+import {insertIntoContainer} from './modules/module-constructor';
+const introTemplate = makeIntroTemplate();
+insertIntoContainer(introTemplate);
