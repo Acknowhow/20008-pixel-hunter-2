8d3a6a8d9e4aa5c4769ef1044dc747a12a211744
Vadim Korabelnik <vad@oblako9.ru>
2018-04-03 16:35:30 +0300

    Mentor's edits revised

--- js/main.js
+++ js/main.js
@@ -1,31 +1,39 @@
-(function () {
+{
   const KEY_CODE_RIGHT = 39;
   const KEY_CODE_LEFT = 37;
+
   const centralContainer = document.querySelector(`.central`);
   const templates = Array.from(document.querySelectorAll(`template`));
   let defaultIndex = 0;
-  function show(slide) {
+
+  const showSlide = (slide) => {
     const content = slide.content;
     const clone = content.cloneNode(true);
+
     centralContainer.innerHTML = ``;
     centralContainer.appendChild(clone);
-  }
-  function keyDownHandler(ev) {
+  };
+
+  const changeSlide = (ev) => {
     if (ev.altKey && ev.keyCode === KEY_CODE_RIGHT) {
       defaultIndex++;
+
       if (defaultIndex % templates.length === 0) {
         defaultIndex--;
       }
     }
+
     if (ev.altKey && ev.keyCode === KEY_CODE_LEFT) {
       defaultIndex--;
+
       if (defaultIndex === -1) {
         defaultIndex++;
       }
     }
-    show(templates[defaultIndex]);
-  }
-  document.addEventListener(`keydown`, keyDownHandler);
-  show(templates[defaultIndex]);
-})();
+    showSlide(templates[defaultIndex]);
+  };
+
+  document.addEventListener(`keydown`, changeSlide);
+  showSlide(templates[defaultIndex]);
+}
 
