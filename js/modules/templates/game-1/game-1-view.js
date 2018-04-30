fad440c4975efb6b31a8a5d8e8e6c40691175ff0
Vadim Korabelnik <vad@oblako9.ru>
2018-04-30 22:37:14 +0300

    Adding game-1 bind

--- js/modules/templates/game-1/game-1-view.js
+++ js/modules/templates/game-1/game-1-view.js
@@ -1,20 +1,23 @@
 import textData from './game-1-data';
 import {drawnAnswers} from '../../handlers/answers';
-import AbstractView from "../../../util/view";
+import AbstractView from '../../../util/view';
+
+import HeaderView from '../header/header-view';
+import {createElement} from "../../../util/contractor";
 
 export default class Game1View extends AbstractView {
-  constructor(state, textData, questionData, answersData) {
+  constructor(state, questionData, answersData) {
     super();
 
     this.state = state;
-    this.textData = textData;
     this.questionData = questionData;
     this.answersData = answersData;
   }
 
   get template() {
     return `
-    <p class="game__task">${this.textData.title}</p>
+    ${new HeaderView(this.state).element}
+    <p class="game__task">${textData.title}</p>
     <form class="game__content">
       ${this.questionData.map(({option, params}) => `<div class="game__option">
         <img src="${params.src}" alt="${option}" width="${params.width}"
@@ -34,16 +37,17 @@ export default class Game1View extends AbstractView {
         ${drawnAnswers(this.answersData)};
       </ul>
     </div>`;
+  }
+
+  onReset() {
 
   }
 
+  render() {
+    return createElement(this.template, `div`, [`game`]);
+  }
 
+  bind() {
 
-
-
-
-  const article = `<div class='game'>${content}</div>`;
-  return makeTemplate(article);
+  }
 }
-
-}}
