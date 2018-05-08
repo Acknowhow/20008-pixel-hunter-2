4a2d9549ab987846aec114a2c67e33d765d2f749
Vadim Korabelnik <vad@oblako9.ru>
2018-05-09 01:43:50 +0300

    Adding dialog view

---
+++ js/modules/templates/dialog/dialog-view.js
@@ -0,0 +1,63 @@
+import AbstractView from '../../../util/view';
+import {createElement} from '../../../util/contractor';
+
+export default class DialogView extends AbstractView {
+  constructor(title, content) {
+    super();
+    this.title = title;
+    this.content = content;
+  }
+
+  get template() {
+    return `
+    <dialog>
+      <form class="dialog-form" autocomplete="off">
+
+        <div class="dialog-header">
+          ${this.title}
+        <span class="dialog-close">×</span>
+        </div>
+
+        <div class="dialog-content">
+          ${this.content}
+        </div>
+
+        <div class="dialog-footer">
+          <button class="button dialog-submit">OK</button>
+          <button class="button dialog-cancel">Отмена</button>
+        </div>
+      </form>
+    </dialog>`;
+  }
+
+  onCancel() {
+
+  }
+
+  onConfirm() {
+
+  }
+
+  bind() {
+    const closeButton = this.element.querySelector(`.dialog-close`);
+    const cancelButton = this.element.querySelector(`.dialog-cancel`);
+    const confirmButton = this.element.querySelector(`.dialog-submit`);
+
+    const cancelHandler = (evt) => {
+      evt.stopPropagation();
+      evt.preventDefault();
+
+      this.onCancel();
+    };
+
+    cancelButton.addEventListener(`click`, cancelHandler);
+    closeButton.addEventListener(`click`, cancelHandler);
+
+    confirmButton.addEventListener(`click`, (evt) => {
+      evt.stopPropagation();
+      evt.preventDefault();
+
+      this.onConfirm();
+    });
+  }
+}
